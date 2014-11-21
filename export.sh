645dc1878d961babc3b0d88ef677fd4f6f36f95f
Alexander-Wilms <f.alexander.wilms@gmail.com>
2014-11-21 03:01:58 +0100

    Add preliminary export script

---
+++ export.sh
@@ -0,0 +1,4 @@
+cp miscellaneous.svg blah.svg ; \
+inkscape -f blah.svg --select lc_iconsetformatdialog --verb=FitCanvasToSelection --verb=LayerDelete --verb=FileSave --verb=FileClose ; \
+inkscape blah.svg -w 24 -h 24 -e lc_iconsetformatdialog ; \
+rm blah.svg
